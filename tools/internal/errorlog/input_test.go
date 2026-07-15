ad41d854fbac94e99fca56766faddfd37132fd2e
Rudi Bruchez <rudi.@babaluga.com>
2026-07-15 15:20:07 +0200

    feat(errorlog): input acquisition (zip/gz/plain/dir/stdin)

---
+++ tools/internal/errorlog/input_test.go
@@ -0,0 +1,41 @@
+package errorlog
+
+import (
+	"archive/zip"
+	"bytes"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestAcquirePlainAndZip(t *testing.T) {
+	dir := t.TempDir()
+
+	plain := filepath.Join(dir, "ERRORLOG")
+	if err := os.WriteFile(plain, encodeUTF16LE("hello\r\n"), 0o644); err != nil {
+		t.Fatal(err)
+	}
+	srcs, err := Acquire(plain)
+	if err != nil || len(srcs) != 1 {
+		t.Fatalf("Acquire plain: %v, n=%d", err, len(srcs))
+	}
+	if Decode(srcs[0].Data) != "hello\n" {
+		t.Errorf("plain data = %q", Decode(srcs[0].Data))
+	}
+
+	zpath := filepath.Join(dir, "ERRORLOG.zip")
+	var buf bytes.Buffer
+	zw := zip.NewWriter(&buf)
+	w, _ := zw.Create("ERRORLOG")
+	w.Write(encodeUTF16LE("zipped\r\n"))
+	zw.Close()
+	os.WriteFile(zpath, buf.Bytes(), 0o644)
+
+	srcs, err = Acquire(zpath)
+	if err != nil || len(srcs) != 1 {
+		t.Fatalf("Acquire zip: %v, n=%d", err, len(srcs))
+	}
+	if Decode(srcs[0].Data) != "zipped\n" {
+		t.Errorf("zip data = %q", Decode(srcs[0].Data))
+	}
+}
